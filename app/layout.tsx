33a97ec4bf5941a1dab53b5baa1c1727a4d5fd6d
adrianvg71 <adrianvg71@educastur.es>
2023-05-28 22:18:39 +0200

    final

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import ActiveStatus from './components/ActiveStatus'
+import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Messenger',
+  description: 'Messenger clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="./favicon.ico" sizes="any" />
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
